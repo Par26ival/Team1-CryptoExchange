96a48688b18f005ab5c85255d518827845be30b4
Par26ival <92543214+Par26ival@users.noreply.github.com>
2025-06-23 19:27:35 +0300

    finished endpoints

--- src/main/java/com/crypto/exchange/prices/service/CryptoPriceService.java
+++ src/main/java/com/crypto/exchange/prices/service/CryptoPriceService.java
@@ -1,28 +1,47 @@
 package com.crypto.exchange.prices.service;
 
 import com.crypto.exchange.prices.dto.PricePointDTO;
+import com.crypto.exchange.prices.projection.DownsampledPriceProjection;
+import com.crypto.exchange.prices.repository.PriceHistoryRepository;
+import com.crypto.exchange.prices.scheduler.PriceFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class CryptoPriceService {
+    @Autowired
+    private PriceHistoryRepository priceHistoryRepository;
+    @Autowired
+    private PriceFetcher priceFetcher;
+
     public Map<String, Map<String, Double>> getAllLatestPrices() {
-        return new HashMap<>();
+        return priceFetcher.getAllLatestPrices();
     }
 
-    public List<PricePointDTO> getPriceHistory(String crypto, String fiat, String from, String to) {
-        return new ArrayList<>();
+    public List<PricePointDTO> getPriceHistory(String crypto, String fiat, String bucketSize, String rangeInterval) {
+        List<DownsampledPriceProjection> projections = priceHistoryRepository.getDownsampledPrices(
+                crypto, fiat, bucketSize, rangeInterval);
+        List<PricePointDTO> history = new ArrayList<>();
+        for (DownsampledPriceProjection proj : projections) {
+            history.add(new PricePointDTO(proj.getBucket(), proj.getAvgPrice()));
+        }
+        return history;
     }
 
     public Double convert(String from, String to, double amount) {
-        return null;
+        Double fromPriceInUSD = getCryptoPriceInFiat(from, "usd");
+        Double toPriceInUSD = getCryptoPriceInFiat(to, "usd");
+        if (fromPriceInUSD == null || toPriceInUSD == null)
+            return null;
+        double usdValue = amount * fromPriceInUSD;
+        return usdValue / toPriceInUSD;
     }
 
     public Double getCryptoPriceInFiat(String crypto, String fiat) {
-        return null;
+        return priceFetcher.getPrice(crypto, fiat);
     }
 }
